2e868c1594c72a32690934295cf2925a50b60c34
kizziah <michael@mobility-labs.com>
2015-01-21 15:09:29 -0500

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mgdavisblog::Application.config.secret_key_base = 'cc179bfd6033b5670d16b49b18a7b7ce2a9133e2d88a6e5daa14ed589bd101188a7b3bc981f6da0e29622c47a9ae2a2132bde523f5ec01f50deb7dcba00dc113'
