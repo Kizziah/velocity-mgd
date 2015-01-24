c413f730a835d9350cd4423794b05d7589dc003f
kizziah <michael@mobility-labs.com>
2015-01-24 15:21:01 -0500

    proper min format for angular

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -9,95 +9,25 @@
  */
 angular.module('myApp', []);
 
-// angular.module('myApp').directive('swing', [
-//     function($compile) {
-//       return {
-//         restrict: 'A',
-//         scope: {},
-//         link: function(scope, elm, attrs) {
-//           scope.remove = function() {            
-//             elm.remove();
-//           };         
+angular.module('myApp').directive('swing', ['$compile',
+    function($compile) {
+      return {
+        restrict: 'A',
+        scope: {},
+        link: function(scope, elm, attrs) {
 
-//           scope.swing = function() {            
-//             $("div.swing").velocity("callout.swing");
+          scope.swing = function() {            
+            $("div.swing").velocity("callout.swing");
 
-//           };
+          };
 
-//           elm.removeAttr("swing"); // Prevents loop
-//           elm.attr('ng-click', "swing()");
-//           $compile(elm)(scope);
-//         },
+          elm.removeAttr("swing"); // Prevents loop
+          elm.attr('ng-click', "swing()");
+          $compile(elm)(scope);
+        },
 
-//      };
-// }]);
-
-// angular.module('myApp').directive('swooping', ['$compile',
-//     function($compile) {
-//       return {
-//         restrict: 'A',
-//         scope: {},
-//         link: function(scope, elm, attrs) {
-//           scope.remove = function() {            
-//             elm.remove();
-//           };         
-
-//           scope.swooping = function() {            
-//             $("div.swooping").velocity("transition.swoopOut");
-//           };
-
-//           elm.removeAttr("swooping"); // Prevents loop
-//           elm.attr('ng-click', "swooping()");
-//           $compile(elm)(scope);
-//         },
-
-//      };
-// }]);
-
-// myApp.directive('bounce', ['$compile',
-//     function($compile) {
-//       return {
-//         restrict: 'A',
-//         scope: {},
-//         link: function(scope, elm, attrs) {
-    
-//           scope.bounce = function() {            
-//             $("div.bounce").velocity("callout.bounce");
-//           };
-
-//           elm.removeAttr("bounce"); // Prevents loop
-//           elm.attr('ng-click', "bounce()");
-//           $compile(elm)(scope);
-//         },
-
-//      };
-// }]);
-
-// myApp.directive('boomerang', ['$compile',
-//     function($compile) {
-//       return {
-//         restrict: 'A',
-//         scope: {},
-//         link: function(scope, elm, attrs) {
-       
-//           scope.boomerang = function() {         
-//             $('div.box').velocity({
-//               rotateY: '360deg'
-//             },{
-//               duration:1000,
-//               easing:'linear'
-//             });
-//           };
-
-//           elm.removeAttr("boomerang"); // Prevents loop
-//           elm.attr('ng-click', "boomerang()");
-//           $compile(elm)(scope);
-//         },
-
-//      };
-// }]);
-
-
+     };
+}]);
 
 angular.module('myApp').directive('myDraggable', ['$document', function($document) {
   return function(scope, element, attr) {
@@ -132,3 +62,66 @@ angular.module('myApp').directive('myDraggable', ['$document', function($documen
     }
   };
 }]);
+
+angular.module('myApp').directive('swooping', ['$compile',
+    function($compile) {
+      return {
+        restrict: 'A',
+        scope: {},
+        link: function(scope, elm, attrs) {
+
+          scope.swooping = function() {            
+            $("div.swooping").velocity("transition.swoopOut");
+          };
+
+          elm.removeAttr("swooping"); // Prevents loop
+          elm.attr('ng-click', "swooping()");
+          $compile(elm)(scope);
+        },
+
+     };
+}]);
+
+angular.module('myApp').directive('bounce', ['$compile',
+    function($compile) {
+      return {
+        restrict: 'A',
+        scope: {},
+        link: function(scope, elm, attrs) {
+    
+          scope.bounce = function() {            
+            $("div.bounce").velocity("callout.bounce");
+          };
+
+          elm.removeAttr("bounce"); // Prevents loop
+          elm.attr('ng-click', "bounce()");
+          $compile(elm)(scope);
+        },
+
+     };
+}]);
+
+// myApp.directive('boomerang', ['$compile',
+//     function($compile) {
+//       return {
+//         restrict: 'A',
+//         scope: {},
+//         link: function(scope, elm, attrs) {
+       
+//           scope.boomerang = function() {         
+//             $('div.box').velocity({
+//               rotateY: '360deg'
+//             },{
+//               duration:1000,
+//               easing:'linear'
+//             });
+//           };
+
+//           elm.removeAttr("boomerang"); // Prevents loop
+//           elm.attr('ng-click', "boomerang()");
+//           $compile(elm)(scope);
+//         },
+
+//      };
+// }]);
+
