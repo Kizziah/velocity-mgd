056990e0918f441d8c30846ab04a27fffc91300c
kizziah <michael@mobility-labs.com>
2015-01-31 13:17:01 -0500

    proper angular

--- app/assets/javascripts/mgdApp.js
+++ app/assets/javascripts/mgdApp.js
@@ -1,22 +1,10 @@
 angular.module("myApp").controller("actionCtrl", [
   "$q", "$scope", "$timeout", "$document", function($q, $scope, $timeout, $document) {
-
-		$timeout(function() {
-			// $('.git').velocity({
-			//   rotateX: '360deg',
-			//   rotateY: '360deg'
-			// },{
-			//   duration: 2500,
-			//   easing:'linear'
-			// });					
+  	
+		function swoop() {
 			$('.about').velocity('transition.swoopIn', 1500);						
-			$('.action-row').velocity('transition.swoopIn', 1500);						
-		});
-
-			function swoop() {
-				alert("Hello");
-		 	// $('.action-row').velocity('transition.swoopIn', 1500);
-			};
+			$('.action-row').velocity('transition.swoopIn', 1500);	
+		};
 
 		$document.on('page:load', swoop());
 
@@ -25,4 +13,3 @@ angular.module("myApp").controller("actionCtrl", [
     };    
   }
 ]);
-
