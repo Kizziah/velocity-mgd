2fbb3da430123a38d42fdb5fc25b7aa702cc543d
kizziah <mkizziah38@gmail.com>
2015-07-13 10:21:09 -0400

    inteval added

--- app/assets/javascripts/mgdApp.js
+++ app/assets/javascripts/mgdApp.js
@@ -1,23 +1,30 @@
 angular.module("myApp").controller("actionCtrl", [
-  "$q", "$scope", "$timeout", "$document", function($q, $scope, $timeout, $document) {
-  	
-    $scope.swoop = function() {
-      $('.about').velocity('transition.swoopIn', 1500);           
-      $('.action-row').velocity('transition.swoopIn', 1500);  
-    };
+  "$q", "$scope", "$timeout", "$document", "$interval", function($q, $scope, $timeout, $document, $interval) {
+    var usaflag = 'assets/flag.png';
+  	var elephantflag = 'assets/map.png';
 
-    $scope.animateCalloutDiv = function(action) {
-      $(".about").velocity("callout." + action);
-    };    
-    
-    $document.ready($scope.swoop);
-    $document.on('page:load', $scope.swoop);
-    
+    $scope.flag = usaflag; 
+    $scope.changeFlag = function() {
+      if($scope.flag == usaflag) {
+        $scope.flag = elephantflag;
+      }
+      else {
+        $scope.flag = usaflag;
+      }     
+    };
+      $interval(function () {
+        
+        $scope.changeFlag();
+      }, 1000);    
   }
 ]);
 
-$(function() {
-  $( "li" ).each(function( index ) {
-    console.log( index + ": " + $( this ).text() );
-  });
-});
+    // $scope.swoop = function() {
+    //   $('.about').velocity('transition.swoopIn', 1500);           
+    //   $('.action-row').velocity('transition.swoopIn', 1500);  
+    // };
+
+    // $scope.animateCalloutDiv = function(action) {
+    //   $(".about").velocity("callout." + action);
+    // };   
+   
